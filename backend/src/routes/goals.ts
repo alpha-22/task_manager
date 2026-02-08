f6c2d76bde243363fb86720315bc7ef13f0f16f7
Parth Jasathy <parth.jasathy@encora.com>
2026-02-09 01:50:21 +0530

    changes to goals.ts

--- backend/src/routes/goals.ts
+++ backend/src/routes/goals.ts
@@ -1,36 +1,38 @@
 import express from "express";
-import { auth } from "../middlewares/authMiddleware";
+import { protect } from "../middlewares/authMiddleware";
 import prisma from "../prisma";
 
 const router = express.Router();
 
-router.get("/", auth, async (req: any, res) => {
+router.get("/", protect, async (req: any, res) => {
   const goals = await prisma.goal.findMany({
-    where: { userId: req.user.id }
+    where: { userId: req.user.id },
   });
   res.json(goals);
 });
 
-router.post("/", auth, async (req: any, res) => {
+router.post("/", protect, async (req: any, res) => {
   const goal = await prisma.goal.create({
     data: {
       title: req.body.title,
-      userId: req.user.id
-    }
+      userId: req.user.id,
+    },
   });
   res.json(goal);
 });
 
-router.put("/:id", auth, async (req, res) => {
+router.put("/:id", protect, async (req, res) => {
   const goal = await prisma.goal.update({
     where: { id: Number(req.params.id) },
-    data: { progress: req.body.progress }
+    data: { progress: req.body.progress },
   });
   res.json(goal);
 });
 
-router.delete("/:id", auth, async (req, res) => {
-  await prisma.goal.delete({ where: { id: Number(req.params.id) } });
+router.delete("/:id", protect, async (req, res) => {
+  await prisma.goal.delete({
+    where: { id: Number(req.params.id) },
+  });
   res.sendStatus(204);
 });
 
